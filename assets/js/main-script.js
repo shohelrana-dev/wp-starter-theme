ff3c1e55a2601f514e80277c4104633e93f405db
Shohel Rana <shohelrrrana@gmail.com>
2020-11-18 00:42:11 +0600

    Update

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,5 +1,16 @@
 ; (function ($) {
     $(document).ready(function () {
-       
+        var macy = Macy({
+            container: '.post-container',
+            trueOrder: false,
+            waitForImages: false,
+            columns: 4,
+            breakAt: {
+                1200: 4,
+                940: 3,
+                520: 2,
+                400: 1
+            }
+        });
     });
 })(jQuery)
